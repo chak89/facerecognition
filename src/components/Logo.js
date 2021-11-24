39333f3f58b2844f240d715799ef8865c94d2ecd
Chak Tang <chakl@hotmail.com>
2021-11-24 12:14:17 +0100

    Installed React-Tilty, styled Logo.js

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Tilty from 'react-tilty';
+import "./Logo.css";
 
 const Logo = () => {
 	return (
 		<div className='ma4 mt0'>
-		
+	      <Tilty className="tilty br2 shadow-2" glare scale={1.05} maxGlare={0.5}>
+	        <div className="inner">Logo</div>
+	      </Tilty>
 		</div>
 	);
 }

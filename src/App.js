375b85b88a54555fcf841a4a07f806a27e07a713
Chak Tang <CHAKL@hotmail.com>
2021-12-01 18:27:26 +0100

    Fixed background and ImageLinkForm

--- src/App.js
+++ src/App.js
@@ -5,91 +5,113 @@ import ImageLinkForm from './components/ImageLinkForm.js';
 import './App.css';
 import Particles from "react-tsparticles";
 
+
+
+//Code for generating particles.
 const particlesOptions = {
-        interactivity: {
-          events: {
-            onClick: {
-              enable: true,
-              mode: "push",
-            },
-            onHover: {
-              enable: true,
-              mode: "repulse",
-            },
-            resize: true,
-          },
-          modes: {
-            bubble: {
-              distance: 400,
-              duration: 2,
-              opacity: 0.8,
-              size: 40,
-            },
-            push: {
-              quantity: 4,
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
-            },
-          },
-        },
-        particles: {
-          color: {
-            value: "#ffffff",
-          },
-          links: {
-            color: "#ffffff",
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
-          },
-          collisions: {
-            enable: true,
-          },
-          move: {
-            direction: "none",
-            enable: true,
-            outMode: "bounce",
-            random: false,
-            speed: 6,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              value_area: 800,
-            },
-            value: 80,
-          },
-          opacity: {
-            value: 0.5,
-          },
-          shape: {
-            type: "circle",
-          },
-          size: {
-            random: true,
-            value: 5,
-          },
-        },
-        detectRetina: true,
-      }
+	interactivity: {
+		events: {
+			onClick: {
+				enable: true,
+				mode: "push",
+			},
+			onHover: {
+				enable: true,
+				mode: "repulse",
+			},
+			resize: true,
+		},
+		modes: {
+			bubble: {
+				distance: 400,
+				duration: 2,
+				opacity: 0.8,
+				size: 40,
+			},
+			push: {
+				quantity: 4,
+			},
+			repulse: {
+				distance: 200,
+				duration: 0.4,
+			},
+		},
+	},
+	particles: {
+		color: {
+			value: "#ffffff",
+		},
+		links: {
+			color: "#ffffff",
+			distance: 150,
+			enable: true,
+			opacity: 0.5,
+			width: 1,
+		},
+		collisions: {
+			enable: true,
+		},
+		move: {
+			direction: "none",
+			enable: true,
+			outMode: "bounce",
+			random: false,
+			speed: 1,
+			straight: false,
+		},
+		number: {
+			density: {
+				enable: true,
+				value_area: 1500,
+			},
+			value: 60,
+		},
+		opacity: {
+			value: 0.5,
+		},
+		shape: {
+			type: "circle",
+		},
+		size: {
+			random: true,
+			value: 5,
+		},
+	},
+	detectRetina: true,
+}
 
 class App extends Component {
-  render() {
-    return (
-          <div className="App">
-            <Particles id="tsparticles"
-          options={particlesOptions}/>
-            <Navigation />
-            <Logo />
-            <ImageLinkForm />
-            {/*<FaceRecognition />*/}
-          </div>
-    );
-  }
+
+	constructor() {
+		super();
+		this.state = {
+			input: ""
+		}
+	}
+
+	onInputChange = (event) => {
+		console.log(event.target.value);
+	}
+
+	onSubmit = () => {
+		console.log('click')
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<Particles id="tsparticles"
+					options={particlesOptions} />
+				<Navigation />
+				<Logo />
+				<ImageLinkForm
+					onInputChange={this.onInputChange}
+					onButtonSubmit={this.onSubmit}
+				/>
+				{/*<FaceRecognition />*/}
+			</div>
+		);
+	}
 }
 
 
--- src/components/ImageLinkForm.js
+++ src/components/ImageLinkForm.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import './ImageLinkForm.css'
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 	return (
 		<div className='ma4 mt0'>
 			<p className='f3'>
-				{'Detect faces in picture'}		
+				{'Detect faces in picture'}
 			</p>
-			<div className='fl w-40'> &nbsp;</div>
-			<div className='form centering fl w-20 pa4 br3 shadow-5'>
-				<input className='' type='text' />
-				<button className='pointer'>Detect</button>
+			<div className='center'>
+				<div className='form center pa4 br3 shadow-5'>
+					<input className='w-80 center' type='text' onChange={onInputChange} />
+					<button
+						className='w-20 grow link pointer bg-light-purple'
+						onClick={onButtonSubmit}>Detect</button>
+				</div>
 			</div>
-			<div className='fl w-40'>&nbsp; </div>
+
 		</div>
 	);
 }
